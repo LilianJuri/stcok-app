db92227a4d4c1187a3b4a1b6938a460484d30551
LilianJuri <Lilianjuri17@gmail.com>
2022-01-21 10:32:37 -0300

    changes index.js and add connection database

---
+++ back-end/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express ();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://lili:1234@cluster0.1sw4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .then((error) => console.log(error.message));
+
+// 
+// 
