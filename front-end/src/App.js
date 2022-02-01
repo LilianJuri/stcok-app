3e774614aecfcc08df21af00ac492a4139d1e956
LilianJuri <73552161+LilianJuri@users.noreply.github.com>
2022-02-01 13:19:43 -0300

    setup front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hola! soy tu app desde el front-end</h1>
+        </div>
+    )
+}
+
+export default App;
